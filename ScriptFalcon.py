66ef6c15c27ea0bc27f87189b0a8c03329fbaa76
lfpiaggio <50186538+lfpiaggio@users.noreply.github.com>
2024-10-25 09:32:32 -0300

    Create ScriptFalcon.py

---
+++ ScriptFalcon.py
@@ -0,0 +1,6 @@
+# Solicita o nome e a data de nascimento do usuário
+nome = input("Por favor, insira seu nome: ")
+data_nascimento = input("Por favor, insira sua data de nascimento (dd/mm/aaaa): ")
+
+# Exibe a informação coletada
+print(f"Olá, {nome}! Você nasceu em {data_nascimento}.")
